a30080feec772306d6f03b6235dbe0136fb2d3d7
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-20 15:09:22 -0400

    added start prompt

--- src/Components/DisplayImages.js
+++ src/Components/DisplayImages.js
@@ -1,8 +1,21 @@
 const DisplayImages = ({ images }) => {
-  const imagesArray = images.map((image) => image)
-  const result = images.length === 0 ? '' : imagesArray
 
-  return <div>{result}</div>
+  const startStyle = {
+    alignItems: 'center',
+    display: 'flex',
+    fontSize: '30px',
+    height: '100vh',
+    justifyContent: 'center',
+
+  }
+  const divStyle = {
+    display: 'block'
+  }
+  const imagesArray = images.map((image) => image)
+  const result = images.length === 0 ? <p>Click anywhere on the screen!</p> : imagesArray
+  const setStyle = images.length === 0 ? startStyle : divStyle
+
+  return <div style={setStyle}>{result}</div>
 }
 
 export default DisplayImages
