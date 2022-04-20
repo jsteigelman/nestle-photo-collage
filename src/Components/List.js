67c34d6b39d77923597eac4fa60e8be5e551d7bf
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-20 13:44:44 -0400

    updated images list

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,11 +1,46 @@
 import gerber from './../images/gerber.png'
 import haagendazs from './../images/haagendazs.png'
 import hotpockets from './../images/hotpockets.png'
+import fancyfeast from './../images/fancyfeast.png'
+import starbucks from './../images/starbucks.png'
+import acquapanna from './../images/acquapanna.png'
+import polandspring from './../images/polandspring.png'
+import sanpellegrino from './../images/sanpellegrino.png'
+
+import perrier from './../images/perrier.png'
+import deerpark from './../images/deerpark.png'
+import tollhouse from './../images/tollhouse.png'
+import carnation from './../images/carnation.png'
+import coffeemate from './../images/coffeemate.png'
+import digiorno from './../images/digiorno.png'
+import buitoni from './../images/buitoni.png'
+import stouffers from './../images/stouffers.png'
+import tidycats from './../images/tidycats.png'
+import alpo from './../images/alpo.png'
+import purina from './../images/purina.png'
+import libbys from './../images/libbys.png'
 
 const List = [
+    acquapanna,
+    alpo,
+    buitoni,
+    carnation,
+    coffeemate,
+    deerpark,
+    digiorno,
+    fancyfeast,
     gerber,
     haagendazs,
-    hotpockets
+    hotpockets,
+    libbys,
+    perrier,
+    polandspring,
+    purina,
+    sanpellegrino,
+    starbucks,
+    stouffers,
+    tidycats,
+    tollhouse
 ]
 
 export default List
