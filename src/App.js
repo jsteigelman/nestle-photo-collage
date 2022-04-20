e960f05bd24a24610a2166d06dce1e6181c13014
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-20 11:53:13 -0400

    clicking on screen displays image at the coordinates of the mouse click event

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import List from './Components/List'
+import Image from './Components/Image'
 import DisplayImages from './Components/DisplayImages'
 
 function App() {
   const [images, setImages] = useState([])
   const [count, setCount] = useState(0)
 
-  const handleAddImage = async () => {
+  const handleAddImage = (event) => {
+
+    // dev testing
+    console.log(`The coords are ${event.clientX}, ${event.clientY}`)
     console.log('Handle add image is running')
+
+    // create new image component
+    const imageSrc = List[count]
+    const newImage = <Image key={Math.floor(Math.random() * 500)} src={imageSrc} x={event.clientX} y={event.clientY} />
+
+    // save image component to list
     let newArray = images.slice()
-    newArray.push(List[count])
-    await setImages(newArray)
+    newArray.push(newImage)
+    setImages(newArray)
 
+    // loop through list of available image files by incrementing count
     const availableImageFiles = List.length
-
-    console.log('Count: ', count)
-    console.log('Available image files: ', availableImageFiles)
-    console.log('Images length: ', images.length)
-
     setCount(count + 1)
-    console.log('Count: ', count)
-    console.log('Images: ', images)
     if (count === availableImageFiles - 1) {
-      console.log('count is equal to avail image files!')
       setCount(0)
     }
-
-    // console.log('handle add image is running!')
-    // let x = 100
-    // let y = 100
-
-    // console.log('test of list count: ', List[count])
-
-    // const nextImage = <Image key={count} src={List[count]} x={x} y={y} />
-    // const arrayTest =
-    //   ((<Image key={count} src={List[count]} x={100} y={100} />),
-    //   (<Image key={count + 1} src={List[count]} x={200} y={200} />))
-
-    // count += 1
-    // let list = images
-    // // list.push(nextImage)
-    // console.log('List is currently: ', list)
-    // // setImages(list)
-    // setImages(arrayTest)
   }
 
   return (
-    <div className='App' onClick={handleAddImage}>
+    <div className='App' onMouseDown={handleAddImage} >
       <DisplayImages images={images} />
     </div>
   )
--- src/Components/DisplayImages.js
+++ src/Components/DisplayImages.js
@@ -1,16 +1,9 @@
 import Image from './Image'
 
 const DisplayImages = ({ images }) => {
+  const imagesArray = images.map((image) => image)
+  const result = images.length === 0 ? '' : imagesArray
 
-  const tempKey = Math.floor(Math.random() * 500)
-  console.log('tempKey is: ', tempKey)
-  console.log('Display images is running')
-  console.log('Images hook length is: ', images.length)
-  console.log('Image files in images hook: ', images, '\n')
-
-  const imagesArray = images.map((image) => <Image key={Math.floor(Math.random() * 500)} src={image} x='200' y='200' />)
-
-  const result = images.length > 0 ? imagesArray : ''
   return <div>{result}</div>
 }
 
