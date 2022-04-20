0dbf186c243252bd4063ec7f11fdcf657592f696
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-20 12:28:04 -0400

    hover brings image into view

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,18 +1,24 @@
+const Image = ({ src, x, y }) => {
+  const imageStyle = {
+    left: x + 'px',
+    top: y + 'px',
+    maxHeight: '300px',
+    maxWidth: '600px',
+    position: 'absolute',
+    transform:
+      'translate(-50%, -50%) scale(0.5) rotate(' +
+      (Math.random() * 20 - 10) +
+      'deg)',
+  }
 
-const Image = ({src, x, y}) => {
-
-    const imageStyle = {
-        left: x + 'px',
-        top: y + 'px',
-        maxHeight: '300px',
-        maxWidth: '600px',
-        position: 'absolute',
-        transform: 'translate(-50%, -50%) scale(0.5) rotate(' + (Math.random() * 20 - 10) + 'deg)' 
-    }
-
-    return (
-        <img style={imageStyle} src={src} alt="Image of a Nestle product"/>
-    )
+  return (
+    <img
+      className='productImage'
+      style={imageStyle}
+      src={src}
+      alt='Image of a Nestle product'
+    />
+  )
 }
 
 export default Image
